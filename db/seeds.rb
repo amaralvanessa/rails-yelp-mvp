ae9febdef9b89e1c2b4cbd6bc006d02058e6c259
Vanessa Moreira <amaralvanessa@gmail.com>
2019-08-21 15:14:00 +0100

    migrate db and populated db with seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create([{ name: 'La Pasta', phone_number: '3333-4444', address: 'Roma street', category: 'italian' }, { name: 'Sushi Yummy', phone_number: '4444-5555', address: 'Tokyo street', category: 'japanese' }, { name: 'China Great Wall', phone_number: '5555-6666', address: 'Shanghai street', category: 'chinese' }, { name: 'Le Creperie', phone_number: '6666-7777', address: 'Paris street', category: 'french' }])
+#   Character.create(name: 'Luke', movie: movies.first)
