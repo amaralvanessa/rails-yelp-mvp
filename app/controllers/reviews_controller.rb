ebbd3f4451bfdf27e524a3a018e719aff4412b99
Vanessa Moreira <amaralvanessa@gmail.com>
2019-08-21 17:03:31 +0100

    finished reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,24 @@
 class ReviewsController < ApplicationController
   def create
+    @review = Review.new(create_review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+
+      if @review.save
+        redirect_to @restaurant, notice: "Review successfully created!"
+      else
+        render :new
+      end
   end
 
   def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  private
+
+  def create_review_params
+    params.require(:review).permit(:content, :rating, :restaurant_id)
   end
 end
